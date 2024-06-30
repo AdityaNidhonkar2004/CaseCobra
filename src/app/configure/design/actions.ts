526a6f466ca558989ece2baa97b5f034adf62735
Atharva Nidhonkar <atharvanidhonkar10@gmail.com>
2024-07-01 00:46:33 +0530

    initial commit

---
+++ src/app/configure/design/actions.ts
@@ -0,0 +1,33 @@
+"use server"
+
+import { db } from "@/db"
+import { CaseColor, CaseFinish, CaseMaterial, PhoneModel } from "@prisma/client"
+
+
+export type SaveConfigArgs = {
+    color: CaseColor,
+    finish: CaseFinish,
+    material: CaseMaterial,
+    model : PhoneModel,
+    configId: string
+}
+
+//RPC -> Remote Procedural Call
+export async function saveConfig({
+  color,
+  finish,
+  material,
+  model,
+  configId,
+}:{
+    color: CaseColor,
+    finish: CaseFinish,
+    material: CaseMaterial,
+    model : PhoneModel,
+    configId: string
+}) {
+    await db.configuration.update({
+        where: {id:configId},
+        data : {color,finish,material,model},
+    })
+} 
---
+++ src/components/Footer.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import MaxWidthWrapper from './MaxWidthWrapper'
+import Link from 'next/link'
+
+const Footer = () => {
+  return (
+    <footer className='bg-white relative mb-5'>
+        <MaxWidthWrapper>
+            <div className='border-t border-gray-200'/>
+            <div className='h-full flex justify-center md:text-left pb-2 md:pb-0'>
+                <p className=' text-sm text-muted-foreground my-5'>
+                    &copy;{new Date().getFullYear()} All rights reserved
+                </p>
+            </div>
+            <div className='flex items-center justify-center'>
+                <div className='flex space-x-8'>
+                    <Link href="#" className='text:sm text-muted-foreground hover:text-gray-600'>
+                    Terms
+                    </Link>
+                    <Link href="#" className='text:sm text-muted-foreground hover:text-gray-600'>
+                    Privacy Policy
+                    </Link>
+                    <Link href="#" className='text:sm text-muted-foreground hover:text-gray-600'>
+                    Cookie Policy
+                    </Link>
+                </div>
+            </div>
+        </MaxWidthWrapper>
+    </footer>
+  )
+}
+
+export default Footer
